b45efbaa809f91df5e1c34f89ccbae6ec202a15a
RyuuSenac <rafael.mgaldino@senacsp.edu.br>
2026-09-17 21:12:25 -0300

    Update db.js

--- Back/src/config/db.js
+++ Back/src/config/db.js
@@ -3,7 +3,7 @@ import mysql from "mysql2/promise.js";
 
 const conexao = mysql.createPool({
     host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT || 3306),
+    port: Number(process.env.DB_PORT),
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
